81bc2ad7435f9e15babd34906367cc22d9bd1dd5
kanedasec <vernon.acs@gmail.com>
2023-09-03 16:30:31 -0300

    cleaning

--- scripts/livehosts.sh
+++ scripts/livehosts.sh
@@ -2,7 +2,7 @@
 
 SELFIP="$(ifconfig | grep inet | grep broadcast | cut -d " " -f 10)"
 TARGETNETWORK="$(echo $SELFIP | sed 's:[^.]*$:0/24:')"
-report_path = "./fuktermux/resources/reports"
+report_path="./fuktermux/resources/reports"
 
 nmap_output=$(nmap -sn $TARGETNETWORK -oG - | grep Up | cut -d " " -f 2)
 
@@ -11,5 +11,5 @@ datetoday=$(date +"%d_%m_%Y")
 read -p "How would you name this network? "  network_name
 
 touch /tmp/live_hosts-$network_name-$datetoday 
-echo $nmap_output |nethunter xargs -n 1 | tee $report_path/live_hosts-$network_name-$datetoday
+echo $nmap_output | xargs -n 1 | tee $report_path/live_hosts-$network_name-$datetoday
 
