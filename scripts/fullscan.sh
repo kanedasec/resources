1e14e5a2e56debdbd020a0baf30084d537d6193f
kanedasec <vernon.acs@gmail.com>
2023-09-03 16:14:51 -0300

    cleaning

--- scripts/fullscan.sh
+++ scripts/fullscan.sh
@@ -2,21 +2,19 @@
 
 SELFIP="$(ifconfig | grep inet | grep broadcast | cut -d " " -f 10)"
 TARGETNETWORK="$(echo $SELFIP | sed 's:[^.]*$:0/24:')"
-
-live_output=$(nethunter nmap -n -sn $TARGETNETWORK -oG - | grep Up | cut -d " " -f 2)
-
 report_path = "./fuktermux/resources/reports"
 
+#check for live hosts and save it in a file
+live_output=$(nmap -n -sn $TARGETNETWORK -oG - | grep Up | cut -d " " -f 2)
+touch /tmp/tmp_live-hosts.txt 
+echo $live_output |xargs -n 1 |tee /tmp/tmp_live-hosts.txt
 
-nethunter touch /tmp/tmp_live-hosts.txt 
-nethunter echo $live_output |nethunter xargs -n 1 |nethunter tee /tmp/tmp_live-hosts.txt
+#scan the hosts enumerated above and save the results in a file
+read -p "How would you name this network? " network_name
+while read -r live_host
+do printf "\n\n Scanning $live_host\n\n"
+nmap -sV --script vulners --script-args mincvss=7.0 -T5 --top-ports 1000 $live_host |tee -a $report_path/Full_Scan-$network_name-$(date +%Y_%m_%d)
+done < /tmp/tmp_live-hosts.txt
 
-echo 'read -p "How would you name this network? "  network_name; while read -r live_host; do printf "\n\n Scanning $live_host\n\n"; nmap -sV --script vulners --script-args mincvss=7.0 -T5 --top-ports 1000 $live_host |tee -a /tmp/Full_Scan-$network_name-$(date +%Y_%m_%d); done < /tmp/tmp_live-hosts.txt' | nethunter tee /tmp/tmp_while-loop.txt
+rm /tmp/tmp_live-hosts.txt
 
-nethunter -r chmod +x /tmp/tmp_while-loop.txt
-
-nethunter /tmp/tmp_while-loop.txt
-
-
-nethunter rm /tmp/tmp_live-hosts.txt
-nethunter rm /tmp/tmp_while-loop.txt
--- scripts/livehosts.sh
+++ scripts/livehosts.sh
@@ -2,13 +2,14 @@
 
 SELFIP="$(ifconfig | grep inet | grep broadcast | cut -d " " -f 10)"
 TARGETNETWORK="$(echo $SELFIP | sed 's:[^.]*$:0/24:')"
+report_path = "./fuktermux/resources/reports"
 
-nmap_output=$(nethunter nmap -sn $TARGETNETWORK -oG - | grep Up | cut -d " " -f 2)
+nmap_output=$(nmap -sn $TARGETNETWORK -oG - | grep Up | cut -d " " -f 2)
 
 datetoday=$(date +"%d_%m_%Y")
 
 read -p "How would you name this network? "  network_name
 
-nethunter touch /tmp/live_hosts-$network_name-$datetoday 
-nethunter echo $nmap_output |nethunter xargs -n 1 |nethunter tee /tmp/live_hosts-$network_name-$datetoday
+touch /tmp/live_hosts-$network_name-$datetoday 
+echo $nmap_output |nethunter xargs -n 1 | tee $report_path/live_hosts-$network_name-$datetoday
 
